33bf3d9f417f8f44fafbdc960da65cacc97f6159
MauGarr <3292719381105@ingenieria.usac.edu.gt>
2024-02-25 19:16:18 -0600

    Se mejora interfaz y se agrega el botón editar, para poder actualizar datos y eliminar colaborador

--- DATOSEMPLEADO_FE/src/components/ColaboradorList.jsx
+++ DATOSEMPLEADO_FE/src/components/ColaboradorList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
-import { getAllColaboradores } from "../api/colaboradores.api";
-import { ColaboradorCard } from "./ColaboradorCard";
+import { getAllColaboradores } from "../api/colaboradores.api"
+import { useNavigate } from "react-router-dom"
 
 export function ColaboradorList() {
 
@@ -14,10 +14,41 @@ export function ColaboradorList() {
         loadColaboradores();
     }, []);
 
-    return <div className="grid grid-cols-3 gap-3">
+    const navigate = useNavigate();    
+
+    return <div className="my-4">
+    <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+        <tr>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Nombre</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Apellido</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Dirección</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Edad</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Profesión</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Estado Civil</th>
+            <th scope="col" className="px-4 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Opciones</th>
+        </tr>
+        </thead>
+        <tbody className="bg-gray divide-y divide-gray-200">
         {colaboradores.map(colaborador => (
-            <ColaboradorCard key={colaborador.idcolaborador} colaborador={colaborador} /> 
+            <tr key={colaborador.idcolaborador}>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.nombre}</td>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.apellido}</td>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.direccion}</td>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.edad}</td>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.profesion}</td>
+            <td className="px-4 py-3 whitespace-nowrap">{colaborador.estadocivil}</td>
+            <td className="px-4 py-3 whitespace-nowrap flex">
+                <button className='bg-violet-600 p-1 rounded-lg block w-auto'>Nivel de Riesgo</button> 
+                <button className='bg-red-600 p-1 rounded-lg block w-auto ml-2'
+                    onClick={() => {navigate(`/colaboradores/${colaborador.idcolaborador}`)}}>
+                    Editar
+                </button> 
+            </td>
+            </tr>
         ))}
+        </tbody>
+    </table>
     </div>;
 }
 
